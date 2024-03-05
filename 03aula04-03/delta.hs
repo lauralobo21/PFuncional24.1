6486b013feef8ca07b55f65448fbb7eb4c82dc61
Laura Lobo <lauralobo364@gmail.com>
2024-03-05 08:33:32 -0300

    aula 04-03

---
+++ 03aula04-03/delta.hs
@@ -0,0 +1,15 @@
+-- eq2g a b c = 
+--     if a == 0 || delta < 0
+--         then 
+--             error "lascou"
+--         else 
+--             (x0, x1)
+--         where
+--         delta = b*b - 4*a*c
+--         x0 = (-b + sqrt delta) / (2*a)
+--         x1 = (-b - sqrt delta) / (2*a)
+-- :l nome do arquivo (compila)
+
+coutSquare n = sum [1 | x <- [1..n], y <- [1..x], z <- [1..y]]
+    where 
+        isSqr a b c = a^2 == b^2 + c^2
---
+++ 03aula04-03/divisivelpor7.hs
@@ -0,0 +1,2 @@
+-- Quantos números menores que 1.000.000 são divisíveis por 7 e não por 5
+const n = sum [ 1 | x <-1..n, mod n 7 == 0, mod n 5 /= 0]
